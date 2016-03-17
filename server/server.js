04470ce50da28a44ea156bf3718dcc338f2a7906
Albert Plana <albert.plana@photobox.com>
2016-03-17 19:35:04 +0000

    Automatically and randomly changing door status

--- server/server.js
+++ server/server.js
@@ -1,26 +1,53 @@
+'use strict';
+
 const http = require('http');
-const server = http.createServer(function(request, response) {});
+const server = http.createServer();
 const WebSocketServer = require('websocket').server;
 const wsServer = new WebSocketServer({
     httpServer: server
 });
 
+let doorStatus = [false, false];
+let automaticChanges = true;
+
+function maybeChangeDoorValue() {
+	let random = Math.random();
+	let doorToChange;
+
+	if (random < 0.5) {
+		// We change one of the doors status
+		random = Math.random();
+
+		doorToChange = random < 0.5 ? 0 : 1;
+		doorStatus[doorToChange] = !doorStatus[doorToChange];
+	}
+	// else We keep everything as it is now
+}
+
+function getReturningData() {
+	return [{
+		id: 'room1',
+		name: 'Left room',
+		available: doorStatus[0]
+	},
+	{
+		id: 'room2',
+		name: 'Right room',
+		available: doorStatus[1]
+	}];
+}
+
 wsServer.on('request', (r) => {
     const connection = r.accept();
+    const randomChanges = setInterval(() => {
+    	if (automaticChanges) {
+	    	maybeChangeDoorValue();
+	    	connection.send(JSON.stringify(getReturningData()));
+	    }
+    }, 10000);
    
     connection.on('message', (message) => {
-    	const data = [{
-    		id: 'room1',
-            name: "Left room",
-            status: false
-        },
-        {
-            id: 'room2',
-            name: "Right room",
-            status: true        
-        }];
-
-    	connection.send(JSON.stringify(data));
+    	connection.send(JSON.stringify(getReturningData()));
 	});
 });
 
