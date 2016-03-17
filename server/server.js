5ef8bf9ee2988d299e1c1f674605df94511627b5
Albert Plana <albert.plana@photobox.com>
2016-03-17 17:18:27 +0000

    Adding websocket

--- server/server.js
+++ server/server.js
@@ -1,17 +1,11 @@
 'use strict';
 
 const Hapi = require('hapi');
-
 const server = new Hapi.Server();
+
 server.connection({ port: 3000 });
 
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
-});
+const io = require('socket.io')(server.listener);
 
 server.route({
     method: 'GET',
@@ -22,10 +16,24 @@ server.route({
 });
 
 server.route({
-    method: 'POST',
+    method: 'GET',
     path: '/sensor-status',
     handler: function (request, reply) {
         reply(true);
     }
 });
 
+io.on('connection', (socket) => {
+    socket.emit('Oh hii!');
+
+    socket.on('burp', function () {
+        socket.emit('Excuse you!');
+    });
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
