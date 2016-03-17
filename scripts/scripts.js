d4464af1a3e4003ac3d2ad4f4eea85ed3ac9ae75
Sara Morueta <Sara.Morueta@moonpig.com>
2016-03-17 22:37:13 +0000

    add button book functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,29 @@
 (function() {
 	var revieversDashBoard = function() {
-		var vm =  this;
+		var vm =  this,
+			roomsJson = [];
+
+		vm.ws = new WebSocket('ws://localhost:3000');
+
 		//initialize flow
 		vm.initialize = function (argument) {
-			vm.openWebSocketConnection();
+			vm.openWebSocketConnection("get-doors-status");
 			vm.defineBookClickEvent();
 		};
 
-		vm.openWebSocketConnection = function () {
-			var ws = new WebSocket('ws://localhost:3000');
+		vm.openWebSocketConnection = function (action) {
 
-			ws.onmessage = (function (event) {
+			vm.ws.onmessage = (function (event) {
 				var self = this;
-				self.roomsJson = JSON.parse(event.data);
-				self.updateUiRoomStatus();
+				if (action === "get-doors-status") {
+					self.roomsJson = JSON.parse(event.data)["door_status"];
+					//self.queue = self.roomsJson.queue;
+					self.updateUiRoomStatus();
+				} else {
+					self.queue = JSON.parse(event.data);				
+					self.updateQueue();
+					action = "get-doors-status"; 
+				}
 			}).bind(vm);
 
 		    this.waitForConnection = function (callback, interval) {
@@ -29,15 +39,17 @@
 			};
 
 			this.waitForConnection(function () {
-		        ws.send(true);
+		        vm.ws.send(action);
 		        if (typeof callback !== 'undefined') {
 		          callback();
 		        }
 		    }, 1000);
 
 		};
+		vm.openWebSocketConnection("get-doors-status");
 
 		vm.updateUiRoomStatus = function() {
+			
 			vm.roomsJson.forEach(function (room){
 				var currentRoom = document.getElementById(room.id);
 				currentRoom.className = room.available ? "room available" : "room";
@@ -45,15 +57,27 @@
 			});
 		};
 
+		vm.updateQueue = function() {
+			vm.yourTurn = vm.queue[vm.queue.length - 1];
+			vm.queueList = "";
+			
+			vm.queue.forEach(function(queueNumber) {
+				var self = this;
+
+				this.queueList += "<li>" + queueNumber + "</li>";
+			}.bind(vm));
+
+			document.getElementsByClassName("queue")[0].innerHTML = vm.queueList;
+		};
+
 		vm.defineBookClickEvent = function (argument) {
 			document.getElementById("book").addEventListener("click", function(e) {
-				debugger;
+				vm.openWebSocketConnection("add-to-queue");
 			});
 		};
 
 		vm.initialize();
 		
 	};
-
 	return revieversDashBoard(); 
 })();
